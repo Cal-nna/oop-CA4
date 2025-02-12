1ab4813b4ca204a6ce989f7fe98d0c5ae0818e5a
Cal <d00269431@student.dkit.ie>
2025-02-12 20:15:40 +0000

    Set up DAO for income and Expenses. Set up a database connection but have yet to test it.

---
+++ src/main/java/dto/Expense.java
@@ -0,0 +1,61 @@
+package dto;
+
+import java.sql.Date;
+
+public class Expense {
+    private int expenseID;
+    private String title;
+    private String category;
+    private double amount;
+
+    public Expense(int expenseID, String title, String category, double amount, Date dateIncurred) {
+        this.expenseID = expenseID;
+        this.title = title;
+        this.category = category;
+        this.amount = amount;
+        this.dateIncurred = dateIncurred;
+    }
+
+    public void setDateIncurred(Date dateIncurred) {
+        this.dateIncurred = dateIncurred;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setExpenseID(int expenseID) {
+        this.expenseID = expenseID;
+    }
+
+    private Date dateIncurred;
+
+
+
+    public int getExpenseID() { return expenseID; }
+    public String getTitle() { return title; }
+    public String getCategory() { return category; }
+    public double getAmount() { return amount; }
+    public Date getDateIncurred() { return dateIncurred; }
+
+
+    @Override
+    public String toString() {
+        return "Expense{" +
+                "expenseID=" + expenseID +
+                ", title='" + title + '\'' +
+                ", category='" + category + '\'' +
+                ", amount=" + amount +
+                ", dateIncurred=" + dateIncurred +
+                '}';
+    }
+}
+
---
+++ src/main/java/dto/Income.java
@@ -0,0 +1,59 @@
+package dto;
+
+import java.util.Date;
+
+public class Income {
+    private int incomeID;
+    private String title;
+    private double amount;
+    private Date dateEarned;
+
+    public Date getDateEarned() {
+        return dateEarned;
+    }
+
+    public void setDateEarned(Date dateEarned) {
+        this.dateEarned = dateEarned;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getIncomeID() {
+        return incomeID;
+    }
+
+    public void setIncomeID(int incomeID) {
+        this.incomeID = incomeID;
+    }
+
+    public Income(int incomeID, double amount, String title, Date dateEarned) {
+        this.incomeID = incomeID;
+        this.amount = amount;
+        this.title = title;
+        this.dateEarned = dateEarned;
+    }
+
+    @Override
+    public String toString() {
+        return "Income{" +
+                "incomeID=" + incomeID +
+                ", title='" + title + '\'' +
+                ", amount=" + amount +
+                ", dateEarned=" + dateEarned +
+                '}';
+    }
+}
